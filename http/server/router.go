2fb5825c642c9b65803430625ecc465ad79c5580
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-04 17:37:41 +0700

    SiGG-Proof-Einstein-Rosen-Bridge-Theory-server-oauth-router.go

---
+++ http/server/router.go
@@ -0,0 +1,133 @@
+package server
+
+/*
+ *
+ *  SiGG-Proof-Einstein-Rosen-Bridge-Theory
+ *
+ */
+ 
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/primasio/wormhole/config"
+	"github.com/primasio/wormhole/http/controllers/api/v1"
+	"github.com/primasio/wormhole/http/middlewares"
+	"github.com/szuecs/gin-glog"
+)
+
+func NewRouter() *gin.Engine {
+
+	gin.DisableConsoleColor()
+
+	router := gin.New()
+	router.Use(ginglog.Logger(3 * time.Second))
+	router.Use(gin.Recovery())
+
+	// CORS config
+	c := config.GetConfig()
+
+	router.Use(middlewares.NewCors(middlewares.CorsConfig{
+		AllowOrigins:     c.GetStringSlice("cors.origins"),
+		AllowMethods:     c.GetStringSlice("cors.methods"),
+		AllowHeaders:     c.GetStringSlice("cors.allowed_headers"),
+		ExposeHeaders:    c.GetStringSlice("cors.exposed_headers"),
+		AllowCredentials: c.GetBool("cors.allow_credentials"),
+	}))
+
+	v1g := router.Group("v1")
+	{
+		// OAuth 2.0 endpoints
+
+		oauthCtrl := new(v1.OAuthController)
+
+		oauthGroup := v1g.Group("oauth")
+		{
+			oauthGroup.GET("google", oauthCtrl.GoogleAuth)
+			oauthGroup.GET("callback/google", oauthCtrl.GoogleAuthCallback)
+		}
+
+		// User endpoints
+
+		userCtrl := new(v1.UserController)
+
+		userGroup := v1g.Group("users")
+		{
+			userGroup.POST("/auth", userCtrl.Auth)
+			userGroup.POST("", userCtrl.Create)
+
+			userGroup.Use(middlewares.AuthMiddleware())
+			{
+				userGroup.GET("", userCtrl.Get)
+			}
+		}
+
+		// Article endpoints
+
+		articleCtrl := new(v1.ArticleController)
+
+		articleGroupAuthorized := v1g.Group("articles").Use(middlewares.AuthMiddleware())
+		{
+			articleGroupAuthorized.GET("/:article_id", articleCtrl.Get)
+			articleGroupAuthorized.POST("", articleCtrl.Publish)
+		}
+
+		// Domain endpoints
+
+		domainController := new(v1.DomainController)
+
+		domainGroup := v1g.Group("domains")
+		{
+			domainGroup.GET("", domainController.List)
+			domainGroup.GET("/domain", domainController.Get)
+		}
+
+		domainGroupAuthorized := v1g.Group("domains").Use(middlewares.AuthMiddleware())
+		{
+			domainGroupAuthorized.POST("", domainController.Create)
+			domainGroupAuthorized.PUT("/domain", domainController.Vote)
+		}
+
+		domainGroupAdmin := v1g.Group("domains").Use(middlewares.AdminAuthMiddleware())
+		{
+			domainGroupAdmin.POST("/domain/approval", domainController.Approve)
+		}
+
+		// URL Content endpoints
+
+		urlContentController := new(v1.URLContentController)
+
+		urlContentGroup := v1g.Group("urls")
+		{
+			urlContentGroup.GET("", urlContentController.List)
+			urlContentGroup.GET("/url", urlContentController.Get)
+		}
+
+		// URL Content Comments endpoints
+
+		urlContentCommentCtrl := new(v1.URLContentCommentController)
+
+		urlContentCommentGroup := v1g.Group("comments")
+		{
+			urlContentCommentGroup.GET("", urlContentCommentCtrl.List)
+		}
+
+		urlContentCommentGroupAuthorized := v1g.Group("comments").Use(middlewares.AuthMiddleware())
+		{
+			urlContentCommentGroupAuthorized.POST("", urlContentCommentCtrl.Create)
+			urlContentCommentGroupAuthorized.DELETE("/:comment_id", urlContentCommentCtrl.Delete)
+
+			urlContentCommentVoteCtrl := new(v1.URLContentCommentVoteController)
+			urlContentCommentGroupAuthorized.POST("/:comment_id/votes", urlContentCommentVoteCtrl.Create)
+			urlContentCommentGroupAuthorized.PUT("/:comment_id/votes", urlContentCommentVoteCtrl.Update)
+			urlContentCommentGroupAuthorized.DELETE("/:comment_id/votes", urlContentCommentVoteCtrl.Delete)
+		}
+
+		authorizedUserGroup := v1g.Group("authorized").Use(middlewares.AuthMiddleware())
+		{
+			authorizedUserGroup.GET("/comments", urlContentCommentCtrl.ListWithVote)
+		}
+	}
+
+	return router
+}
