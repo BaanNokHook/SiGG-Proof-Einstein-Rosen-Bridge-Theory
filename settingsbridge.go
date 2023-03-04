8b5c1d528f90f600e502a12a58b6ede629472302
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-04 13:06:21 +0700

    SiGG-Proof-Einstein-Rosen-Bridge-Theory-Series-settingsbridge.go

---
+++ settingsbridge.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+ *
+ *  SiGG-Proof-Einstein-Rosen-Bridge-Theory
+ *
+ */
+
+import (
+	"fmt"
+
+	"github.com/therecipe/qt/core"
+)
+
+type SettingsBridge struct {
+	core.QObject
+
+	_ func()         `constructor:"init"`
+	_ func(s string) `signal:"onServerAddressUpdate,auto"`
+	_ func(s string) `signal:"onDownloadDirectoryUpdate,auto"`
+
+	_ string `property:"serverAddress"`
+	_ string `property:"downloadDirectory"`
+}
+
+func (b *SettingsBridge) init() {
+	fmt.Printf("Settings init")
+
+	b.SetServerAddress("")
+	b.SetDownloadDirectory("")
+
+	SettingsBridge = b
+}
+
+func (b *SettingsBridge) onServerAddressUpdate(s string) {
+	//fmt.Printf("Server Address: %s", s)
+	b.SetServerAddress(s)
+}
+
+func (b *SettingsBridge) onDownloadDirectoryUpdate(s string) {
+	//fmt.Printf("Download directory: %s", s)
+	b.SetDownloadDirectory(s)
+}
