6720df77a3a1284ff1e36ae8ef87517131b5e2fd
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-04 12:15:21 +0700

    SiGG-Proof-Einstein-Rosen-Bridge-Theory-Series-main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+/*
+ *
+ *  SiGG-Proof-Einstein-Rosen-Bridge-Theory
+ *
+ */
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/quick"
+	"github.com/therecipe/qt/widgets"
+)
+
+var (
+	receiveBridge  *ReceiveBridge
+	senderBridge   *SenderBridge
+	settingsBridge *SettingsBridge
+
+	receiverTableModel *RecvFileTableModel
+	senderTableModel   *SendFileTableModel
+)
+
+const BufferSize = 1024
+
+func init() {
+	RecvFileTableModel_QmlRegisterType2("CustomQmlTypes", 1, 0, "RecvFileTableModel")
+	SendFileTableModel_QmlRegisterType2("CustomQmlTypes", 1, 0, "SendFileTableModel")
+	ReceiveBridge_QmlRegisterType2("CustomQmlTypes", 1, 0, "ReceiveBridge")
+	SenderBridge_QmlRegisterType2("CustomQmlTypes", 1, 0, "SendBridge")
+	SettingsBridge_QmlRegisterType2("CustomQmlTypes", 1, 0, "SettingsBridge")
+}
+
+func main() {
+	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
+
+	receiverTableModel = NewRecvFileTableModel(nil)
+	senderTableModel = NewSendFileTableModel(nil)
+
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+	view := quick.NewQQuickView(nil)
+
+	view.SetTitle("Rosen")
+	view.SetResizeMode(quick.QQuickView__SizeRootObjectToView)
+	view.SetSource(core.NewQUrl3("qrc:/qml/rosen.qml", 0))
+	view.Show()
+
+	app.Exec()
+}
