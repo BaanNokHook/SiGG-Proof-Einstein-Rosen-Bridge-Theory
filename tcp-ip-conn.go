d7ed69128ba5470d5e46b2cd3a07e84661261a61
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-05 18:30:03 +0700

    SiGG-Proof-Einstein-Rosen-Bridge-Theory-Series-TCP/IP-Netwok-tcp-ip-conn.go

---
+++ tcp-ip-conn.go
@@ -0,0 +1,25 @@
+package base
+
+/*
+ *
+ *  SiGG-Proof-Einstein-Rosen-Bridge-Theory
+ *
+ */
+
+import (
+	"context"
+	"net"
+
+	"github.com/hedzr/log"
+)
+
+type Conn interface {
+	Logger() log.Logger
+
+	Close()
+
+	RemoteAddr() net.Addr
+
+	// RawWrite does write through the internal net.Conn
+	RawWrite(ctx context.Context, message []byte) (n int, err error)
+}
