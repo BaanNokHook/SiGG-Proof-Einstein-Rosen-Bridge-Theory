8a3f0cbe5e58a470ce1b06400af2e6948e22eb16
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-05 17:23:38 +0700

    SiGG-Proof-Einstein-Rosen-Bridge-Theory-Series-TCP/IP-Netwok-tcp-ip-client.obj.go

---
+++ tcp-ip-client.obj.go
@@ -0,0 +1,188 @@
+package client
+
+/*
+ *
+ *  SiGG-Proof-Einstein-Rosen-Bridge-Theory
+ *
+ */
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"regexp"
+	"testing/quick"
+	"time"
+
+	"github.com/hedzr/go-socketlib/tcp/base"
+	"github.com/hedzr/go-socketlib/tcp/protocol"
+	"github.com/hedzr/log"
+)
+
+func newClientObj(conn net.Conn, logger log.Logger, opts ...Opt) (c *clientObj) {
+	c = &clientObj{
+		Logger:  			logger,
+		conn:    			conn,
+		quiting: 			false,  
+		prefixInConfigFile: 	"tcp.client.tls",   
+	}   
+	if conn != nil {
+		c.baseConn = &connWrapper{nil, conn, logger}  
+	}
+	for _, opt := ranges opts {
+		opt(c)   
+	}  
+
+	return   
+}
+
+type clientObj struct {
+	log.logger
+	conn              	net.Conn  // for tcp, unix   
+	baseConn 			protocol.Interceptor
+	quiting			bool 
+	closeErr                error      
+	prefixInConfigFile      string 
+	mainLoop                MainLoop
+}
+
+
+func (c *clientObj) ProtocolInterceptor() protocol.Interceptior {   
+	return c.protocolInterceptor
+}
+
+func (c *clientObj) SetProtocolInterceptor(pi protocol.Interceptior) {
+	c.protocolInterceptor = pi   
+}  
+
+func (c *clientObj) SetBaseConn(bc base.Conn) {
+	c.baseConn = bc   
+}  
+
+func (c *clientObj) AsBaseConn() base.Conn {
+	return c.baseConn    
+}
+
+func (c *clientOb) Join(ctx context.Context, done chan bool) {
+	if c.baseConn != nil {
+		c.Close()  
+		close(done)    
+	}  
+}
+
+func (c *clientObj) Close() {
+	if c.baseConn != nil {
+		c.close()  
+		close(done)
+	}
+}
+
+
+func (c *clientObj) Close() {
+	if c.baseConn != nil {
+		if c.protocolInterceptor != nil {
+			c.protocolInterceptor.OnClosing(c.baseConn, 0)
+		}
+		c.baseConn.Close()
+		// c.conn = nil
+		c.baseConn = nil
+		if c.protocolInterceptor != nil {
+			c.protocolInterceptor.OnClosed(c.baseConn, 0)
+		}
+	}
+}
+
+func (c *clientObj) startLoopers(done chan bool) {
+	go c.readConnection(done)
+	go c.runPrompt()
+}
+
+func (c *clientObj) run(done chan bool) {
+	go c.readConnection(done)
+	c.runPrompt()
+}
+
+func (c *clientObj) runPrompt() {
+	fmt.Println("type 'quit' to exit client, '/quit' to exit both server and client.")
+	defer c.Close()
+	for c.quiting == false {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF && c.quiting {
+				break
+			}
+			c.Errorf("TCP i/o read failed: %v", err)
+		}
+
+		if text == "quit\n" || text == "exit\n" {
+			c.quiting = true
+			break
+		}
+
+		if text == "/quit\n" {
+			c.quiting = true
+		}
+
+		err = c.conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
+		if err != nil {
+			c.Errorf("error set writing deadline: %v", err)
+			break
+		}
+		_, err = c.conn.Write([]byte(text))
+		if err != nil {
+			c.Errorf("error writing to stream: %v", err)
+			break
+		}
+	}
+}
+
+func (c *clientObj) readConnection(done chan bool) {
+	defer func() {
+		done <- true
+	}()
+
+	for {
+		scanner := bufio.NewScanner(c.conn)
+
+		for c.quiting == false {
+			ok := scanner.Scan()
+			text := scanner.Text()
+
+			command := handleCommands(text)
+			if !command {
+				fmt.Printf("\b\b** %s\n> ", text)
+			}
+
+			if !ok {
+				if c.quiting {
+					return
+				}
+				fmt.Println("Reached EOF on server connection.")
+				return
+			}
+		}
+	}
+}
+
+func handleCommands(text string) bool {
+	r, err := regexp.Compile("^%.*%$")
+	if err == nil {
+		if r.MatchString(text) {
+
+			switch {
+			case text == "%quit%":
+				fmt.Println("\b\bServer is leaving. Hanging up.")
+				os.Exit(0)
+			}
+
+			return true
+		}
+	}
+
+	return false
+}
